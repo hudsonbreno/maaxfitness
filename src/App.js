5b2c72026dd6ecfa138e701cd67c4f298367f35a
Breno <brennoohudson@gmail.com>
2023-03-28 16:24:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { BrowserRouter, Router, Routes } from "react-router-dom";
+// import HomePage from "";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar>
+        <img src=""/>
+      </Navbar>
+    flor
+    </BrowserRouter>
+
+    {/* <Navbar>
+        <img src=""/>
+
+      </Navbar>
+      <Routes>
+        <Router path="/" element={<HomePage/>}/>
+
+      </Routes>
+      <Footer>Maax Fitness </Footer> */}
+    </>
   );
 }
 
-export default App;
+const Navbar = styled.div`
+display: flex;
+`
+
+// const Footer = styled.div`
+// display: flex;
+// `
